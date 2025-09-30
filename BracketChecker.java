edda0c2c9e7cec3376a1545c9c551263284960cb
Hawraa <hawra8516@gmail.com>
2025-09-30 14:43:32 +0400

    Stack Questions: Question 3 added

---
+++ BracketChecker.java
@@ -0,0 +1,25 @@
+import java.util.Stack;
+
+class BracketChecker {
+    /**
+     * TO-DO: Refactor this method to remove its dependency on the three helper methods
+     * (isOpening, isClosing, isMatchingPair) by inlining their logic.
+     */
+    public boolean isBalancedRefactored(String expr) {
+        Stack<Character> stack = new Stack<>();
+        for (char c : expr.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else if (c == ')' || c == '}' || c == ']') {
+                if (stack.isEmpty()) {
+                    return false;
+                }
+                char top = stack.pop();
+                if (c == ')' && top != '(' || c == '}' && top != '{' || c == ']' && top != '[') {
+                    return false;
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
