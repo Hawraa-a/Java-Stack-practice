14bd01a6120020eceb87c62f46e919afa2e362b2
Hawraa <hawra8516@gmail.com>
2025-10-02 09:13:22 +0400

    Stack Questions.2: Question.3: Basic HTML Tag Validator

---
+++ HtmlValidator.java
@@ -0,0 +1,32 @@
+import java.util.Stack;
+
+class HtmlValidator {
+    /**
+     * TO-DO: Check if an array of HTML tags is well-formed.
+     * \*
+     *
+     * @param tags An array of strings, where each string is a tag like "<html>" or "</html>".
+     * @return true if the tags are properly nested and balanced, false otherwise.
+     */
+    public boolean isWellFormed(String[] tags) {
+        Stack<String> tagStack = new Stack<>();
+
+        for (String tag : tags) {
+            if (tag.startsWith("</")) { // This is a closing tag
+                String tagName = tag.substring(2, tag.length() - 1);
+                if (tagStack.isEmpty()) {
+                    return false;
+                }
+                String openTagName = tagStack.pop();
+                if (!openTagName.equals(tagName)) {
+                    return false;
+                }
+
+            } else { // This is an opening tag
+                String tagName = tag.substring(1, tag.length() - 1);
+                tagStack.push(tagName);
+            }
+        }
+        return tagStack.isEmpty();
+    }
+}
